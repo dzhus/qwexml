b0a6b87f0a25a7fc251e8e51073f272a95445e55
Dmitry Dzhus <dima@sphinx.net.ru>
2009-12-24 14:49:34 +0300

    Incremental parsing demo.

--- qweparsetest.cpp
+++ qweparsetest.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
+#include <sstream>
 #include "qweparse.hpp"
 
 using namespace qwe;
+
+std::string finished_string(XmlParser *p)
+{
+    return ((p->is_finished()) ? "FINISHED" : "UNFINISHED");
+}
 
 /**
  * Read XML from standard input and print it back to standard output.
  */
 int main()
 {
+    const int buf_size = 128;
+    
     XmlParser *p = new XmlParser();
-    std::cin >> *p;
+    char buffer[buf_size];
+    std::istringstream *is;
 
-    TokenList::StlIterator i = p->tokens_begin(), end = p->tokens_end();
-
-    while (i != end)
+    while (std::cin.getline(buffer, buf_size))
     {
-        std::cout << (*i)->get_contents();
-        i++;
+        is = new std::istringstream();
+        is->str(buffer);
+        *is >> *p;
+
+        if(p->top())
+        {
+            std::cout << "<< " << finished_string(p) << ": " << p->top()->get_printable();
+            std::cout << std::endl;
+        }
     }
     return 0;
 }
